b251175ab83b6999193a8e4f671c1486ed1547e0
Ben Ari Maymon <benarimaymon@gmail.com>
2024-12-19 09:00:55 +0200

    commit

---
+++ src/logic/user_logic.py
@@ -0,0 +1,30 @@
+class UserLogic:
+    def __init__(self):
+        pass
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def like_vacation(self):
+        pass
+
+    def unlike_vacation(self):
+        pass
+
+    def view_liked_vacations(self):
+        pass
+
+    def add_new_vacation(self):
+        pass
+
+    def edit_existing_vacation(self):
+        pass
+
+    def delete_vacation(self):
+        pass
---
+++ src/logic/vacation_logic.py
@@ -0,0 +1,15 @@
+class VacationLogic:
+    def __init__(self):
+        pass
+
+    def get_all_vacations(self):
+        pass
+
+    def add_vacation(self, title, description, start_date, end_date, countries_name, price, image):
+        pass
+
+    def edit_vacation(self, id, **kwargs):
+        pass
+
+    def delete_vacation(self, id):
+        pass
